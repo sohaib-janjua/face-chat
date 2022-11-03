db3c54b60905f52410e6533870a26d50931199ed
github_username <mastermind.sohaib786@gmail.com>
2022-11-03 19:00:56 +0500

    commit 11

--- lib/views/home/map_tab.dart
+++ lib/views/home/map_tab.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:login_signup_auth/core/const.dart';
 
 class MapTab extends StatefulWidget {
   const MapTab({super.key});
@@ -13,13 +15,25 @@ class MapTab extends StatefulWidget {
 }
 
 class _MapTabState extends State<MapTab> {
-  LatLng myPosition = LatLng(30.672425377080838, 73.64876633444932);
+  LatLng myPosition = const LatLng(30.672425377080838, 73.64876633444932);
 
   final String uid = FirebaseAuth.instance.currentUser!.uid;
+
+  late BitmapDescriptor mapIcon;
+
+  LatLng destination = const LatLng(30.672425377080838, 73.64876633444932);
 
   @override
   void initState() {
     super.initState();
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration.empty,
+      "assets/images/map_icon.png",
+    ).then((value) {
+      setState(() {
+        mapIcon = value;
+      });
+    });
     getPermissionAndLocation();
   }
 
@@ -34,11 +48,23 @@ class _MapTabState extends State<MapTab> {
       (event) {
         FirebaseFirestore.instance.collection('users').doc(uid).update({
           'location': GeoPoint(event.latitude, event.longitude),
+          'is_live': true,
         });
+
+        mapController.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(p.latitude, p.longitude),
+            zoom: 16,
+          ),
+        ));
       },
     );
     FirebaseFirestore.instance
         .collection('users')
+        .where(
+          'is_live',
+          isEqualTo: true,
+        )
         .snapshots()
         .listen((collect) {
       for (var doc in collect.docs) {
@@ -46,11 +72,13 @@ class _MapTabState extends State<MapTab> {
           var geo = doc.data()['location'] as GeoPoint;
           setState(() {
             markers[doc.id] = Marker(
-                markerId: MarkerId(doc.id),
-                position: LatLng(geo.latitude, geo.longitude),
-                infoWindow: InfoWindow(
-                  title: doc.data()['name'],
-                ));
+              markerId: MarkerId(doc.id),
+              position: LatLng(geo.latitude, geo.longitude),
+              infoWindow: InfoWindow(
+                title: doc.data()['name'],
+              ),
+              //icon: mapIcon,
+            );
           });
         }
       }
@@ -58,17 +86,110 @@ class _MapTabState extends State<MapTab> {
   }
 
   Map<String, Marker> markers = {};
+  Map<String, Polyline> pollyLines = {};
+
+  late GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: const CameraPosition(
-        target: LatLng(30.672425377080838, 73.64876633444932),
-        zoom: 16,
-      ),
-      markers: markers.values.toSet(),
-      myLocationEnabled: true,
-      padding: const EdgeInsets.symmetric(vertical: 50),
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(30.672425377080838, 73.64876633444932),
+            zoom: 16,
+          ),
+          markers: markers.values.toSet(),
+          polylines: pollyLines.values.toSet(),
+          myLocationEnabled: true,
+          mapType: MapType.satellite,
+          onCameraIdle: () {
+            print("On Camera Stop");
+          },
+          onCameraMove: (position) {
+            log(position.target.latitude.toString());
+          },
+          onTap: (argument) {
+            markers['destination'] = Marker(
+              markerId: const MarkerId('destination'),
+              position: argument,
+            );
+            setState(() {
+              destination = argument;
+            });
+          },
+          zoomControlsEnabled: false,
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          onMapCreated: (controller) {
+            mapController = controller;
+            controller.setMapStyle(Const.mapStyle);
+          },
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () async {
+                Position p = await Geolocator.getCurrentPosition();
+                mapController.animateCamera(CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: LatLng(p.latitude, p.longitude),
+                    zoom: 16,
+                  ),
+                ));
+              },
+              icon: const Icon(
+                Icons.atm,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                mapController.setMapStyle(Const.mapNightStyle);
+              },
+              icon: const Icon(
+                Icons.night_shelter,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                PolylinePoints polylinePoints = PolylinePoints();
+                Position p = await Geolocator.getCurrentPosition();
+
+                var results = await polylinePoints.getRouteBetweenCoordinates(
+                  "AIzaSyBE0ICUo4vIKNYv90657DD1qqm7YQQg",
+                  PointLatLng(p.latitude, p.longitude),
+                  PointLatLng(destination.latitude, destination.longitude),
+                );
+
+                pollyLines['route-1'] = Polyline(
+                  polylineId: const PolylineId('route-1'),
+                  color: Colors.redAccent,
+                  width: 12,
+                  endCap: Cap.roundCap,
+                  startCap: Cap.roundCap,
+                  points: results.points
+                      .map(
+                        (e) => LatLng(e.latitude, e.longitude),
+                      )
+                      .toList(),
+                );
+                setState(() {});
+                //results.points;
+              },
+              icon: const Icon(
+                Icons.route,
+              ),
+            ),
+          ],
+        )
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    FirebaseFirestore.instance.collection('users').doc(uid).update({
+      'is_live': false,
+    });
+    super.dispose();
   }
 }
