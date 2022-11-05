1b6520941bf6cc7203ee9cfbee30fa3e5f64b31b
github_username <mastermind.sohaib786@gmail.com>
2022-11-05 19:55:20 +0500

    commit 14

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -2,12 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:login_signup_auth/views/auth/login_screen.dart';
 import 'package:login_signup_auth/views/home/add_post.dart';
 import 'package:login_signup_auth/views/home/map_tab.dart';
 import 'package:login_signup_auth/views/home/post_tab.dart';
 import 'package:login_signup_auth/views/inbox/inbox_view.dart';
 import '../../core/app_navigator.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -21,18 +25,58 @@ class _HomeScreenState extends State<HomeScreen> {
 
   int pageIndex = 0;
 
+  //local notifications variables
+  AndroidInitializationSettings androidLNInit =
+      const AndroidInitializationSettings('app_icon');
+
+  final DarwinInitializationSettings iosLNInit = DarwinInitializationSettings(
+    onDidReceiveLocalNotification: (id, title, body, payload) {},
+    requestSoundPermission: true,
+    requestBadgePermission: true,
+    requestAlertPermission: true,
+  );
+
+  late AndroidNotificationChannel channel;
+  late NotificationDetails notificationDetails;
   @override
   void initState() {
+    super.initState();
+
+    channel = const AndroidNotificationChannel(
+      'com.example.login_signup_auth',
+      'notifications',
+      description: 'This channel is used for important notifications',
+      importance: Importance.high,
+    );
+
+    notificationDetails = NotificationDetails(
+      android: AndroidNotificationDetails(
+        channel.id,
+        channel.name,
+      ),
+    );
+
+    flutterLocalNotificationsPlugin.initialize(InitializationSettings(
+      android: androidLNInit,
+      iOS: iosLNInit,
+    ));
+
     FirebaseMessaging.instance.getToken().then((token) {
       FirebaseFirestore.instance.doc("users/$uid").update({
         'fcm_token': token,
       });
     });
 
-    super.initState();
     FirebaseMessaging.onMessage.listen((msg) {
       String title = msg.notification!.title!;
       String body = msg.notification!.body!;
+
+      flutterLocalNotificationsPlugin.show(
+        msg.hashCode,
+        title,
+        body,
+        notificationDetails,
+      );
       print("ONMESSAGE $title,$body,${msg.data.toString()}");
     });
 
@@ -63,7 +107,7 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButton: pageIndex == 0
           ? FloatingActionButton(
               onPressed: () {
-                appNavPush(context, const AddPostView());
+                // appNavPush(context, const AddPostView());
               },
               child: const Icon(Icons.add),
             )
